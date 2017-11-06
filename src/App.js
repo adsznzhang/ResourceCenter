f8f22ed24f892bf03feb43137a366e2f864c42af
adsznzhang <adszn@hotmail.com>
2017-11-06 22:11:12 +0800

    Add route and theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
-import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import fusTheme from './fusTheme.js';
+
+
 injectTapEventPlugin();
 
 class App extends Component {
@@ -14,9 +18,13 @@ class App extends Component {
     super(props);
     this.state = {open: false};
   }
-  
+
+  handleClose = () => this.setState({open: false});
+
   render() {
     return (
+      <BrowserRouter>
+      <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
       <div className="App">
       <AppBar
       title="Title"
@@ -27,10 +35,44 @@ class App extends Component {
       docked={false}
       onRequestChange={(open) => this.setState({open})}
       >
-      <MenuItem>Menu Item</MenuItem>
-      <MenuItem>Menu Item 2</MenuItem>
+
+
+      <NavLink to='/'><MenuItem onTouchTap={this.handleClose}>Home</MenuItem></NavLink>
+      <NavLink  activeClassName='active' to='/logos'><MenuItem onTouchTap={this.handleClose}>Logos</MenuItem></NavLink>
+      <NavLink to='/posters'><MenuItem onTouchTap={this.handleClose}>Poster</MenuItem></NavLink>
+      <NavLink to='/letterhead'><MenuItem onTouchTap={this.handleClose}>Letterhead</MenuItem></NavLink>
+      <NavLink to='/share-a-story'><MenuItem onTouchTap={this.handleClose}>Share a Story</MenuItem></NavLink>
+      <NavLink to='/services'><MenuItem onTouchTap={this.handleClose}>Sevrvice Request Form</MenuItem></NavLink>
+      <NavLink to='/tutorial'><MenuItem onTouchTap={this.handleClose}>Tutorial</MenuItem></NavLink>
+      
         </Drawer>
+
+        <Route exact path='/' render={() => <h1>Home View </h1>} />
+
+        <Route exact path='/logos'
+          render={()=> <h1>Logos and Posters Videw</h1>}/>
+        <Route exact path='/posters' render={() => <h1>Poster View </h1>}/>
+
+        <Route exact path='/letterhead'
+          render={() => <h1>Letterhead View</h1>} />
+
+        <Route exact path='/share-a-story'
+          render={() => <h1>Share a story View</h1>}/>
+
+        <Route exact path='/planning-guide'
+          render={() => <h1>Planning Guide View</h1>}/>
+
+        <Route exact path='/services'
+          render={()=> <h1> Services View</h1>}/>
+
+        <Route exact path='/glossary'
+          render={() => <h1>Glossary View</h1>}/>
+
+        <Route exact path='/tutorial'
+          render={()=> <h1>Tutorial View</h1>}/>
       </div>
+      </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
-import {cyan500} from 'material-ui/styles/colors';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: '#21412a',
-  },
-  appBar: {
-    height: 50,
-  },
-});
 
-ReactDOM.render(<MuiThemeProvider muiTheme={muiTheme}><App /></MuiThemeProvider>, document.getElementById('root'));
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
