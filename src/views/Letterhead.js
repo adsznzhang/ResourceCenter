6f1e8eabf2fb64f48095d778df13ebd97e87bfb1
adsznzhang <adszn@hotmail.com>
2017-11-11 22:14:56 +0800

    Add letterhead and poster layout

--- src/views/Letterhead.js
+++ src/views/Letterhead.js
@@ -1,8 +1,68 @@
 import React, {Component} from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
 
 class Letterhead extends Component {
     render() {
-        return <h1>Letterhead View</h1>;
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col s12">
+              <p>Lores sdff sdf</p>
+            </div>
+          </div>
+        <div className="row">
+          <div className="col s12 m6">
+         <Card>
+           <CardMedia>
+      <img src="https://picsum.photos/600/776/?random" alt="" />
+    </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle" />
+    <CardActions>
+      <FlatButton label="Action1" />
+         </CardActions>
+        </Card>   
+          </div>
+          <div className="col s12 m6">
+            <Card>
+              <CardMedia>
+      <img src="https://picsum.photos/600/776/?random" alt="" />
+    </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle" />
+    <CardActions>
+    <FlatButton label="Action1" />
+    </CardActions>
+        </Card>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12 m6">
+         <Card>
+           <CardMedia>
+      <img src="https://picsum.photos/600/776/?random" alt="" />
+    </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle" />
+    <CardActions>
+      <FlatButton label="Action1" />
+         </CardActions>
+        </Card>   
+          </div>
+          <div className="col s12 m6">
+            <Card>
+              <CardMedia>
+      <img src="https://picsum.photos/600/776/?random" alt="" />
+    </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle" />
+    <CardActions>
+    <FlatButton label="Action1" />
+    </CardActions>
+        </Card>
+          </div>
+        </div>
+
+        </div>
+      );
     };
 };
 
