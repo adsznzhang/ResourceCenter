f1e1402ef33b6cfa9595d5811f7de5a7c82752ff
adsznzhang <adszn@hotmail.com>
2017-11-13 23:09:47 +0800

    Add story & letterhead view

--- src/views/Letterhead.js
+++ src/views/Letterhead.js
@@ -1,69 +1,57 @@
-import React, {Component} from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-
+import React, { Component } from 'react'
+import LetterheadCard from '../LetterheadCard'
 
 class Letterhead extends Component {
-    render() {
-      return (
-        <div className="container">
-          <div className="row">
-            <div className="col s12">
-              <p>Lores sdff sdf</p>
-            </div>
-          </div>
-        <div className="row">
-          <div className="col s12 m6">
-         <Card>
-           <CardMedia>
-      <img src="https://picsum.photos/600/776/?random" alt="" />
-    </CardMedia>
-            <CardTitle title="Card title" subtitle="Card subtitle" />
-    <CardActions>
-      <FlatButton label="Action1" />
-         </CardActions>
-        </Card>   
-          </div>
-          <div className="col s12 m6">
-            <Card>
-              <CardMedia>
-      <img src="https://picsum.photos/600/776/?random" alt="" />
-    </CardMedia>
-            <CardTitle title="Card title" subtitle="Card subtitle" />
-    <CardActions>
-    <FlatButton label="Action1" />
-    </CardActions>
-        </Card>
+  render () {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col s12'>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </div>
         </div>
-        <div className="row">
-          <div className="col s12 m6">
-         <Card>
-           <CardMedia>
-      <img src="https://picsum.photos/600/776/?random" alt="" />
-    </CardMedia>
-            <CardTitle title="Card title" subtitle="Card subtitle" />
-    <CardActions>
-      <FlatButton label="Action1" />
-         </CardActions>
-        </Card>   
-          </div>
-          <div className="col s12 m6">
-            <Card>
-              <CardMedia>
-      <img src="https://picsum.photos/600/776/?random" alt="" />
-    </CardMedia>
-            <CardTitle title="Card title" subtitle="Card subtitle" />
-    <CardActions>
-    <FlatButton label="Action1" />
-    </CardActions>
-        </Card>
-          </div>
+        <div className='row'>
+          <LetterheadCard
+            title='Card title'
+            action='Action1'
+            image='https://unsplash.it/600/776/?random=1'
+          />
+          <LetterheadCard
+            title='Card title'
+            action='Action1'
+            image='https://unsplash.it/600/776/?random=2'
+          />
+          <LetterheadCard
+            title='Card title'
+            action='Action1'
+            image='https://unsplash.it/600/776/?random=3'
+          />
+          <LetterheadCard
+            title='Card title'
+            action='Action1'
+            image='https://unsplash.it/600/776/?random=4'
+          />
         </div>
+      </div>
+    )
+  }
+}
 
-        </div>
-      );
-    };
-};
-
-export default Letterhead;
+export default Letterhead
--- src/views/Story.js
+++ src/views/Story.js
@@ -1,8 +1,65 @@
 import React, {Component} from 'react';
+import '../styles/responsive-text.css';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Story extends Component {
     render() {
-        return <h1>Story View</h1>;
+      return (
+        <div className='container'>
+        <div className='row'>
+          <div className='col s12 flow-text'>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col s12 m6'>
+            <TextField
+      hintText="Hint Text"
+      floatingLabelText="Floating Label Text"
+        fullWidth
+    />
+          </div>
+          <div className='col s12 m6'>
+            <TextField
+      hintText="Hint Text"
+      floatingLabelText="Floating Label Text"
+              fullWidth
+    />
+          </div>
+          <div className='col s12 '>
+            <TextField
+      hintText="Message Field"
+      floatingLabelText="MultiLine and FloatingLabel"
+              fullWidth
+      multiLine={true}
+      rows={2}
+    />
+          </div>
+          <div className='col s12'>
+            <RaisedButton label="Submit" primary={true} />
+          </div>
+        </div>
+      </div>
+
+      )
     };
 }
 export default Story;
