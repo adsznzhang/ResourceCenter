803f6edf5dfb73d2870081b4d8ab8a45e2d1b2d8
adsznzhang <adszn@hotmail.com>
2018-06-16 22:44:11 +1200

    Add test

--- src/views/Home.test.js
+++ src/views/Home.test.js
@@ -1,2 +1,28 @@
 import {visit} from './testUtils'
 
+describe('Home Page', () => {
+    it('loads on /', async() => {
+        const page= visit('/')
+        const text = await page.evaluate(()=> document.body.textContent).end();
+
+
+        expect(text).not.toContain('Page not found');
+    });
+
+    it('contains "Resource Center"',async () =>{
+        const page = visit('/');
+        const text = await page.evaluate(() => document.body.textContent).end();
+
+        expect(text).toContain('Resource Center');
+    });
+
+
+    it('shows 6 cards', async() =>{
+        const page = visit('/');
+        const selector = '.container .col.s12.m4';
+        const cardCount = await page.wait(selector).evaluate(sel  => document.querySelector(sel).length, selector).end();
+
+        expect(cardCount).toEqual(6);
+    });
+});
+
